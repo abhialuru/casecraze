d6a61a5038db0a718c4ea31852c4fa77396b458c
abhialuru <alurabhi9346@gmail.com>
2025-01-05 13:10:42 +0530

    final cors

---
+++ src/lib/cors.ts
@@ -0,0 +1,24 @@
+// lib/cors.ts
+import Cors from 'cors';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// Initialize the CORS middleware
+const cors = Cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
+  origin: '*', // Set to '*' for all domains or specify your domain e.g. 'https://casecraze-9yzl.vercel.app'
+});
+
+// Helper function to run the middleware
+export const runCorsMiddleware = (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  return new Promise<void>((resolve, reject) => {
+    cors(req, res, (result) => {
+      if (result instanceof Error) {
+        reject(result);
+      }
+      resolve();
+    });
+  });
+};
