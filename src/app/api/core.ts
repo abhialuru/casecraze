ea98c30e883fb4b58f1c2580afe04cf6832f6e83
abhialuru <alurabhi9346@gmail.com>
2025-01-05 17:34:14 +0530

    cors errors

--- src/app/api/core.ts
+++ src/app/api/core.ts
@@ -12,7 +12,7 @@ const cors = Cors({
 function runMiddleware(
   req: NextApiRequest,
   res: NextApiResponse,
-  fn: Function,
+   fn: any
 ) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result: any) => {
