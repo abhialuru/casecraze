16bf6e6383e1f2eb29d09395f95f328ad8a44e68
abhialuru <alurabhi9346@gmail.com>
2025-01-06 17:29:45 +0530

    server error

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,6 @@ const nextConfig: NextConfig = {
   images:{
     domains:['files.edgestore.dev']
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-        ],
-      },
- ];
- },
 };
 
 export default nextConfig;
