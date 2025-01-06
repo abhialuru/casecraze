0ee2828779a746fa185e71d066a7053d06d50a7d
abhialuru <alurabhi9346@gmail.com>
2025-01-06 22:43:09 +0530

    CaseSite

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,15 +1,6 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-// Allow handling of OPTIONS requests (preflight)
-export async function OPTIONS(req:any, res:any) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization, X-Api-Version');
-  res.status(200).end(); // Respond with 200 OK
-}
-
-// Your main GET handler for authentication
-export const GET = handleAuth();
+ export const GET = handleAuth();
 
 
  
